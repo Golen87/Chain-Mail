f14c4ac727f6548394a4bbe8c91d82748d06906d
Joakim Nilsson <joani718@student.liu.se>
2018-01-27 22:12:01 +0100

    getting parm from url

--- facebookApiExample.js
+++ facebookApiExample.js
@@ -2,18 +2,12 @@ window.onload = function () {
     alert("Hello");
     var url_string = window.location.href;
     var url = new URL(url_string);
-    var a = url.searchParams.get("opening");
-    var b = url.searchParams.get("content");
-    var c = url.searchParams.get("closing");
+    var a = url.searchParams.get("message");
 
     a = a.replace(/%/gi, " ");
-    b = b.replace(/%/gi, " ");
-    c = c.replace(/%/gi, " ");
 
     console.log(a);
-    console.log(b);
-    console.log(c);
-
+    
     function postToFacebook(){
         var url = "https://golen87.github.io/Chain-Mail/";
         var message = "He ho asodiasdhji";
