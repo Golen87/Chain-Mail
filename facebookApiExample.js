bcb26366831dbd0380296c9ea0591685c85aee06
Joakim Nilsson <joani718@student.liu.se>
2018-01-27 21:31:06 +0100

    getting parm from url

--- facebookApiExample.js
+++ facebookApiExample.js
@@ -4,8 +4,13 @@ var url = new URL(url_string);
 var a = url.searchParams.get("opening");
 var b = url.searchParams.get("content");
 var c = url.searchParams.get("closing");
+
+a = a.replace(/%/gi, " ");
+b = b.replace(/%/gi, " ");
+c = c.replace(/%/gi, " ");
+
 console.log(a);
 console.log(b);
 console.log(c);
-}
+};
 
