67a83b415275ec10a3b9ff93ece19a1776540e1c
Axel Nordanskog <axeno840@student.liu.se>
2018-01-27 12:45:58 +0100

    Selected window always on top

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ let shares = 0;
 $(document).ready(() => {
     $("#icon1").click(() => {
         $("#mail_gen").show();
+        $("#mail_gen").css('z-index', 2);
+        $("#marketplace").css('z-index', 1);
     });
 
     $("#icon2").click(() => {
         $("#marketplace").show();
+        $("#mail_gen").css('z-index', 1);
+        $("#marketplace").css('z-index', 2);
+    });
+
+    $("#mail_gen").mousedown(() => {
+        $("#mail_gen").css('z-index', 2);
+        $("#marketplace").css('z-index', 1);
+    });
+
+    $("#marketplace").mousedown(() => {
+        $("#mail_gen").css('z-index', 1);
+        $("#marketplace").css('z-index', 2);
     });
 
     $("#mail_close").click(() => {
