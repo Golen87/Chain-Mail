deac459e0ff99397a8db1f85a4f253da705d768c
Golen <golen87@hotmail.com>
2018-01-27 17:46:24 +0100

    Moved graph.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let money = 10000;
 let shares = 0;
 let mail_addresses = 10;
+var current_mail = null;
+var sent_mails = [];
+
 
 function PutOnTop(windowId) {
     var newTop = $(windowId);
@@ -21,7 +24,6 @@ function PutOnTop(windowId) {
     newTop.css('z-index', WINDOWS.length);
 };
 
-const graphColor = "#34e722";
 
 $(document).ready(() => {
     $("#icon1").click(() => {
@@ -77,7 +79,7 @@ $(document).ready(() => {
     updateStats();
 
     let ctx = document.getElementById("chart").getContext('2d');
-    let data = makeSpread(200, 0.7, 10);
+    let data = makeSpread(200, 0.7, 100);
     makeGraph(ctx, data);
 });
 
@@ -87,38 +89,4 @@ function updateStats(){
     $("#mail_disp").text(mail_addresses);
 
     console.log("ran");
-}
-
-function makeGraph(canvas, data){
-    let N = data.length;
-    let labels = Array.apply(null, {length: N}).map(Number.call, Number);
-
-    let myChart = new Chart(canvas, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: data,
-                borderColor: graphColor,
-                fill: false
-            }]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        max: N,
-                        min: 0,
-                        stepSize: 1
-                    }
-                }]
-            },
-            tooltips: {
-                enabled: false
-            },
-            legend: {
-                display: false
-            }
-        }
-    });
 }
