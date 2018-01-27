f70be156dfe3efa52c664ca50b32ea237a78a2f3
Joakim Nilsson <joani718@student.liu.se>
2018-01-27 21:34:07 +0100

    getting parm from url

--- facebookApiExample.js
+++ facebookApiExample.js
@@ -1,16 +1,17 @@
 window.onload = function () {
+    alert("Hello")
     var url_string = window.location.href;
-var url = new URL(url_string);
-var a = url.searchParams.get("opening");
-var b = url.searchParams.get("content");
-var c = url.searchParams.get("closing");
+    var url = new URL(url_string);
+    var a = url.searchParams.get("opening");
+    var b = url.searchParams.get("content");
+    var c = url.searchParams.get("closing");
 
-a = a.replace(/%/gi, " ");
-b = b.replace(/%/gi, " ");
-c = c.replace(/%/gi, " ");
+    a = a.replace(/%/gi, " ");
+    b = b.replace(/%/gi, " ");
+    c = c.replace(/%/gi, " ");
 
-console.log(a);
-console.log(b);
-console.log(c);
+    console.log(a);
+    console.log(b);
+    console.log(c);
 };
 
